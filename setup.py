805aa7426c6638d23d7545db8d401a5220d15c59
Minakshi Burman <117364504+Minakshi843@users.noreply.github.com>
2024-04-06 18:08:10 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Minakshi Burman ',
+    author_email= 'minakshiburman2003@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
